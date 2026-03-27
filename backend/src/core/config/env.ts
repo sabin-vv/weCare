15adf194a5204607ba418d56e654909a9fa5cef6
sabin-vv <vvsabin@gmail.com>
2026-03-27 16:35:00 +0530

    chore : setup the ioredis and nodemailer

--- backend/src/core/config/env.ts
+++ backend/src/core/config/env.ts
@@ -6,4 +6,6 @@ export const env = {
     PORT: process.env.PORT || '5000',
     MONGO_URI: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/weCare',
     JWT_SECRET: process.env.JWT_SECRET || '',
+    EMAIL_USER: process.env.EMAIL_USER || 'vvsabin@gmail.com',
+    EMAIL_PASSWORD: process.env.EMAIL_PASSWORD || 'uinkkccprbykwpdf',
 }
