8a135a00a7b5ac6cbf429c4e9a71639562fcbe82
sabin-vv <vvsabin@gmail.com>
2026-03-28 12:51:12 +0530

    feat: update password  repository and interface are updated

--- backend/src/modules/auth/interfaces/user.repository.interface.ts
+++ backend/src/modules/auth/interfaces/user.repository.interface.ts
@@ -1,6 +1,11 @@
+import { Types } from 'mongoose'
+
 import { UserDocument } from '../types/auth.types'
 
 export interface IUserRepository {
     findByEmail(email: string): Promise<UserDocument | null>
+
     create(data: Partial<UserDocument>): Promise<UserDocument>
+
+    updatePassword(userId: Types.ObjectId, password: string): Promise<void>
 }
--- backend/src/modules/auth/repository/user.repository.ts
+++ backend/src/modules/auth/repository/user.repository.ts
@@ -1,3 +1,4 @@
+import { Types } from 'mongoose'
 import { injectable } from 'tsyringe'
 
 import { BaseRepository } from '../../../core/base/base.repository'
@@ -9,6 +10,9 @@ import { UserDocument } from '../types/auth.types'
 export class UserRepository extends BaseRepository<UserDocument> implements IUserRepository {
     constructor() {
         super(UserModel)
+    }
+    async updatePassword(userId: Types.ObjectId, password: string): Promise<void> {
+        await this.model.findByIdAndUpdate(userId, { password })
     }
     async findByEmail(email: string) {
         return this.model.findOne({ email })
