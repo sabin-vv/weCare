c33acbe3b4820df468a555859e6440e563f78a73
sabin-vv <vvsabin@gmail.com>
2026-03-28 21:37:49 +0530

    initialize frontend with base structure

---
+++ frontend/eslint.config.js
@@ -0,0 +1,46 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js, import: importPlugin }, extends: ["js/recommended"], languageOptions: { globals: globals.browser }, rules: {
+            "import/order": [
+                "error",
+                {
+                    groups: [
+                        "builtin",
+                        "external",
+                        "internal",
+                        "parent",
+                        "sibling",
+                        "index"
+                    ],
+                    "newlines-between": "always",
+                    alphabetize: {
+                        order: "asc",
+                        caseInsensitive: true
+                    }
+                }
+            ],
+            "no-console": ["warn", { allow: ["warn", "error"] }],
+        }
+    },
+
+    tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+
+    {
+        settings: {
+            react: {
+                version: "detect"
+            },
+
+        },
+        rules: {
+            "react/react-in-jsx-scope": "off"
+        }
+    },
+
+]);
