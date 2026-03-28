28be943f0d62d59029d9d523ce11efd441efe288
sabin-vv <vvsabin@gmail.com>
2026-03-28 10:32:56 +0530

    fix : register doctor route fix with addingupload and fix typo

--- backend/src/modules/auth/routes/auth.route.ts
+++ backend/src/modules/auth/routes/auth.route.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { container } from 'tsyringe'
 
+import { upload } from '../../../core/middleware/upload'
 import { validate } from '../../../core/middleware/validateMiddleware'
 import { AuthController } from '../controller/auth.controller'
+import { loginSchema } from '../validator/auth.schema'
 import { registerDoctorSchema } from '../validator/doctor.schema'
 import { SendOtpSchema, verifyOtpSchema } from '../validator/sendOtp.schema'
 
@@ -13,7 +15,13 @@ export const createAuthRoutes = () => {
 
     router.post('/send-otp', validate(SendOtpSchema), authController.sendOtp)
     router.post('/verify-otp', validate(verifyOtpSchema), authController.verifyOtp)
-    router.post('/register-dictor', validate(registerDoctorSchema), authController.registerDoctor)
+    router.post('/login', validate(loginSchema), authController.login)
+    router.post(
+        '/register-doctor',
+        upload.any(),
+        validate(registerDoctorSchema),
+        authController.registerDoctor,
+    )
 
     return router
 }
