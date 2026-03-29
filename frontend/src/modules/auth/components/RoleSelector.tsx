1ecf031d77fa6fde70779fe07a979bbaf768d224
sabin-vv <vvsabin@gmail.com>
2026-03-29 20:34:33 +0530

    feat: role selector component added

---
+++ frontend/src/modules/auth/components/RoleSelector.tsx
@@ -0,0 +1,34 @@
+import { Role, type RoleSelectorProps } from '../types/auth.types'
+import styles from './RoleSelector.module.css'
+
+const RoleSelector = ({ role, onChange }: RoleSelectorProps) => {
+    return (
+        <div className={styles.roleSelectorWrapper}>
+            <p>I am signing in as a:</p>
+            <div className={styles.roleSelector}>
+                <button
+                    type="button"
+                    className={role === 'doctor' ? styles.active : ''}
+                    onClick={() => onChange(Role.DOCTOR)}
+                >
+                    Doctor
+                </button>
+                <button
+                    type="button"
+                    className={role === 'caregiver' ? styles.active : ''}
+                    onClick={() => onChange(Role.CAREGIVER)}
+                >
+                    Caregiver
+                </button>
+                <button
+                    type="button"
+                    className={role === 'patient' ? styles.active : ''}
+                    onClick={() => onChange(Role.PATIENT)}
+                >
+                    Patient
+                </button>
+            </div>
+        </div>
+    )
+}
+export default RoleSelector
--- frontend/src/modules/auth/types/auth.types.ts
+++ frontend/src/modules/auth/types/auth.types.ts
@@ -4,3 +4,15 @@ export interface ProgressBarProps {
     percentage: number
     title: string
 }
+
+export enum Role {
+    DOCTOR = 'doctor',
+    CAREGIVER = 'caregiver',
+    PATIENT = 'patient',
+    ADMIN = 'admin',
+}
+
+export interface RoleSelectorProps {
+    role: Role
+    onChange: (role: Role) => void
+}
