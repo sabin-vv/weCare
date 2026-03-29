f89f82ce4405e04a0ca429a5373cda9766508e23
sabin-vv <vvsabin@gmail.com>
2026-03-30 01:19:01 +0530

    feat: auth header added

---
+++ frontend/src/modules/auth/components/Header.tsx
@@ -0,0 +1,60 @@
+import { useNavigate } from 'react-router-dom'
+
+import styles from './Header.module.css'
+
+import Button from '@/shared/components/Button/Button'
+import { useAuth } from '@/shared/context/AuthContext'
+import { useLogout } from '../hooks/useLogout'
+import { House } from 'lucide-react'
+import { Stethoscope } from 'lucide-react'
+
+const Header = () => {
+    const navigate = useNavigate()
+    const { isAuthenticated, user } = useAuth()
+    const handleLogout = useLogout()
+
+    const initials =
+        user?.name
+            ?.split(' ')
+            .map((part) => part[0])
+            .join('')
+            .slice(0, 2)
+            .toUpperCase() || 'U'
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.headerContainer}>
+                <div className={styles.logo} onClick={() => navigate('/')}>
+                    <img src="" alt="logo" />
+                </div>
+
+                <nav className={styles.navLinks}>
+                    <button className={styles.navLink} onClick={() => navigate('/')}>
+                        <House /> Home
+                    </button>
+                    <button className={styles.navLink} onClick={() => navigate('/appointments')}>
+                        <Stethoscope />
+                        Book an Appointment
+                    </button>
+                </nav>
+
+                <div className={styles.actionButtons}>
+                    {isAuthenticated ? (
+                        <>
+                            <button className={styles.logoutBtn} onClick={handleLogout}>
+                                Logout
+                            </button>
+                            <div className={styles.profileInfo}>
+                                <div className={styles.avatar}>{initials}</div>
+                            </div>
+                        </>
+                    ) : (
+                        <Button onClick={() => navigate('/auth/login')}>Login</Button>
+                    )}
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
