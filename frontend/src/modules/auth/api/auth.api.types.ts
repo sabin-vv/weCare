4e5b62bda8b01b002fdd5cb55f0fa794e24071e0
sabin-vv <vvsabin@gmail.com>
2026-03-30 01:14:59 +0530

    fix: rename files properly

--- frontend/src/modules/auth/api/auth.api.types.ts
+++ frontend/src/modules/auth/api/auth.api.types.ts
@@ -1,34 +1,14 @@
-import { api } from '@/services/api'
-import type { ApiInterface, LoginUser } from './auth.api'
+import type { Role } from '../types/auth.types'
 
-export const sendOtp = async (email: string, purpose: string): Promise<ApiInterface> => {
-    const res = await api.post('/auth/send-otp', {
-        email,
-        purpose,
-    })
-
-    return res.data
+export interface ApiInterface {
+    success: boolean
+    message: string
 }
 
-export const verifyOtp = async (email: string, otp: string): Promise<ApiInterface> => {
-    const res = await api.post('/auth/verify-otp', {
-        email,
-        otp,
-    })
-    return res.data
-}
-
-export const doctorRegister = async (formData: FormData): Promise<ApiInterface> => {
-    const res = await api.post('/doctors/register', formData)
-    return res.data
-}
-
-export const loginUser = async (email: string, password: string, role: string): Promise<LoginUser> => {
-    const res = await api.post('/auth/login', { email, password, role })
-    return res.data
-}
-
-export const logout = async (): Promise<ApiInterface> => {
-    const res = await api.post('/auth/logout')
-    return res.data
+export interface LoginUser extends ApiInterface {
+    user: {
+        name: string
+        email: string
+        role: Role
+    }
 }
