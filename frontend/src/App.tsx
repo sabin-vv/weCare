83f4943a794e303480e06f9ab92dc527c78b8e69
sabin-vv <vvsabin@gmail.com>
2026-03-30 02:17:37 +0530

    feat:added route provider and auth provider in app file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
 import { Toaster } from 'react-hot-toast'
+import { AuthProvider } from './shared/context/AuthContext'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router'
 
 function App() {
-    return <Toaster position="top-center" />
+    return (
+        <AuthProvider>
+            <Toaster position="top-center" />
+            <RouterProvider router={router} />
+        </AuthProvider>
+    )
 }
 
 export default App
