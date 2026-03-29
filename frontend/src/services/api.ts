5b2a8886f8951164e6f2515ed25b79065c3cedb9
sabin-vv <vvsabin@gmail.com>
2026-03-29 18:24:18 +0530

    feat: added axios interceptor

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,18 @@
+import { HTTP_STATUS } from '@/shared/constants/http'
 import axios from 'axios'
 
 export const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
 })
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === HTTP_STATUS.UNAUTHORIZED) {
+            window.location.href = '/auth/login'
+        }
+
+        return Promise.reject(error)
+    },
+)
