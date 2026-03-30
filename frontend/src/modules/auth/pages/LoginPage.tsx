fe93e56f98eb6c19985d4c38e81e7e1a37264ef8
sabin-vv <vvsabin@gmail.com>
2026-03-31 00:20:10 +0530

    feat: create LoginPage component

---
+++ frontend/src/modules/auth/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import AuthLayout from '@/layout/AuthLayout'
+import LoginForm from '../form/login/LoginForm'
+
+const LoginPage = () => {
+    return (
+        <AuthLayout>
+            <LoginForm />
+        </AuthLayout>
+    )
+}
+
+export default LoginPage
