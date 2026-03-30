971ba05c3336f257fc7025ea072f28a8f2c650b9
sabin-vv <vvsabin@gmail.com>
2026-03-30 23:08:28 +0530

    feat: token updated for caregiver repository

--- backend/src/container/tokens.ts
+++ backend/src/container/tokens.ts
@@ -2,6 +2,8 @@ export const TOKENS = {
     IUserRepository: Symbol('IUserRepository'),
     IDoctorRepository: Symbol('IDoctorRepository'),
     IDoctorService: Symbol('IDoctorService'),
+    ICaregiverRepository: Symbol('ICaregiverRepository'),
+    ICaregiverService: Symbol('ICaregiverService'),
     IAuthService: Symbol('IAuthService'),
     IOtpService: Symbol('IOtpService'),
 }
