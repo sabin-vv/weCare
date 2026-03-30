4984f07bbfbaea71387c518a5fa5731baff85c81
sabin-vv <vvsabin@gmail.com>
2026-03-30 19:51:45 +0530

    fix: update doctor controller without parsing files to service

--- backend/src/modules/doctor/controller/doctor.controller.ts
+++ backend/src/modules/doctor/controller/doctor.controller.ts
@@ -3,23 +3,7 @@ import { inject, injectable } from 'tsyringe'
 
 import { TOKENS } from '../../../container/tokens'
 import { HTTP_STATUS } from '../../../core/constants/httpStatus'
-import { MulterFiles } from '../../auth/types/auth.types'
 import { IDoctorService } from '../interfaces/doctor.service.interface'
-
-const normalizeMulterFiles = (files: unknown): MulterFiles => {
-    if (!files) return {}
-
-    if (Array.isArray(files)) {
-        return (files as Express.Multer.File[]).reduce<MulterFiles>((acc, file) => {
-            const key = file.fieldname
-            if (!acc[key]) acc[key] = []
-            acc[key].push(file)
-            return acc
-        }, {})
-    }
-
-    return files as MulterFiles
-}
 
 @injectable()
 export class DoctorController {
@@ -27,7 +11,7 @@ export class DoctorController {
 
     registerDoctor = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const result = await this.doctorService.registerDoctor(req.body, normalizeMulterFiles(req.files))
+            const result = await this.doctorService.registerDoctor(req.body, {})
             res.status(HTTP_STATUS.CREATED).json({ success: true, data: result })
         } catch (error) {
             next(error)
