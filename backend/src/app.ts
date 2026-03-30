8ee963ee21ac62dd0e026df40ea3df613578bceb
sabin-vv <vvsabin@gmail.com>
2026-03-30 18:10:59 +0530

    feat:added a route for file upload

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import express from 'express'
 import { errorMiddleware } from './core/middleware/errorMiddleware'
 import { createAuthRoutes } from './modules/auth/routes/auth.route'
 import { createDoctorRoutes } from './modules/doctor/routes/doctor.route'
+import { createUploadsRoutes } from './modules/uploads/routes/uploads.route'
 
 const app = express()
 
@@ -19,6 +20,8 @@ app.use(express.json())
 app.use('/api/auth', createAuthRoutes())
 
 app.use('/api/doctors', createDoctorRoutes())
+
+app.use('/api/uploads', createUploadsRoutes())
 
 app.use(errorMiddleware)
 
