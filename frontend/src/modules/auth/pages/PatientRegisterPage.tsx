b499d764680b240fb678f2069eacebe803a2f32a
sabin-vv <vvsabin@gmail.com>
2026-03-31 01:28:58 +0530

    feat: create PatientRegisterPage component

---
+++ frontend/src/modules/auth/pages/PatientRegisterPage.tsx
@@ -0,0 +1,12 @@
+import AuthLayout from '@/layout/AuthLayout'
+import PatientRegisterForm from '../form/patient/PatientRegisterForm'
+
+const PatientRegisterPage = () => {
+    return (
+        <AuthLayout>
+            <PatientRegisterForm />
+        </AuthLayout>
+    )
+}
+
+export default PatientRegisterPage
