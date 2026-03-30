84871c52b66127b8ce3b6fac3f6b6ee8cac95993
sabin-vv <vvsabin@gmail.com>
2026-03-30 23:05:50 +0530

    feat: caregiver register service added

---
+++ backend/src/modules/caregiver/service/caregiver.service.ts
@@ -0,0 +1,31 @@
+import { inject, injectable } from 'tsyringe'
+
+import { TOKENS } from '../../../container/tokens'
+import { HTTP_STATUS } from '../../../core/constants/httpStatus'
+import { AppError } from '../../../core/errors/AppError'
+import { IUserRepository } from '../../auth/interfaces/user.repository.interface'
+import { toUserEntity } from '../../auth/mapper/auth.mapper'
+import { MulterFiles } from '../../auth/types/auth.types'
+import { ICaregiverRepository } from '../interfaces/caregiver.repository.interface'
+import { ICaregiverService } from '../interfaces/caregiver.service.interface'
+import { toCaregiverEntity } from '../mapper/caregiver.mapper'
+import { RegisterCaregiverDTO } from '../validator/caregiver.schema'
+
+@injectable()
+export class CaregiverService implements ICaregiverService {
+    constructor(
+        @inject(TOKENS.IUserRepository) private userRepo: IUserRepository,
+        @inject(TOKENS.ICaregiverRepository) private caregiverRepo: ICaregiverRepository,
+    ) {}
+
+    async registerCaregiver(dto: RegisterCaregiverDTO, files: MulterFiles) {
+        const existing = await this.userRepo.findByEmail(dto.email)
+        if (existing) throw new AppError(HTTP_STATUS.BAD_REQUEST, 'User already exist')
+
+        const userData = await toUserEntity(dto)
+        const user = await this.userRepo.create(userData)
+
+        const caregiverData = toCaregiverEntity(user._id, dto, files)
+        return this.caregiverRepo.create(caregiverData)
+    }
+}
