6d46f062115f06406a1f74f9b34f13e6d40b09da
sabin-vv <vvsabin@gmail.com>
2026-03-31 00:19:50 +0530

    feat: add login route to auth routes

--- frontend/src/modules/auth/routes/auth.routes.tsx
+++ frontend/src/modules/auth/routes/auth.routes.tsx
@@ -1,8 +1,13 @@
 import type { RouteObject } from 'react-router-dom'
 import CaregiverRegisterPage from '../pages/CaregiverRegisterPage'
 import DoctorRegisterPage from '../pages/DoctorRegisterPage'
+import LoginPage from '../pages/LoginPage'
 
 export const AuthRoutes: RouteObject[] = [
+    {
+        path: '/auth/login',
+        element: <LoginPage />,
+    },
     {
         path: '/auth/doctors/register',
         element: <DoctorRegisterPage />,
