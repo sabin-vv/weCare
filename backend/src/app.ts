d61c38d2f01c8f97a8b4d43b123355b83d0e3579
sabin-vv <vvsabin@gmail.com>
2026-03-30 13:11:38 +0530

    chore: cors implemented in in backend properly

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,17 @@
+import cors from 'cors'
 import express from 'express'
 
 import { errorMiddleware } from './core/middleware/errorMiddleware'
 import { createAuthRoutes } from './modules/auth/routes/auth.route'
 
 const app = express()
+
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        credentials: true,
+    }),
+)
 
 app.use(express.json())
 
