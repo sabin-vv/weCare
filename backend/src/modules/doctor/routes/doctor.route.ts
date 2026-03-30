18c8b674c9db0d3878935443f489eca21f44c2e0
sabin-vv <vvsabin@gmail.com>
2026-03-30 19:48:05 +0530

    fix: updated the route without file

--- backend/src/modules/doctor/routes/doctor.route.ts
+++ backend/src/modules/doctor/routes/doctor.route.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express'
 import { container } from 'tsyringe'
 
-import { upload } from '../../../core/middleware/upload'
 import { validate } from '../../../core/middleware/validateMiddleware'
 import { DoctorController } from '../controller/doctor.controller'
 import { registerDoctorSchema } from '../validator/registerDoctor.schema'
@@ -10,7 +9,7 @@ export const createDoctorRoutes = () => {
     const router = Router()
     const doctorController = container.resolve(DoctorController)
 
-    router.post('/register', upload.any(), validate(registerDoctorSchema), doctorController.registerDoctor)
+    router.post('/register', validate(registerDoctorSchema), doctorController.registerDoctor)
 
     return router
 }
