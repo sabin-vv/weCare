a917b440ec23c2ee35faf5d7d3b0cd2d6491c3c3
sabin-vv <vvsabin@gmail.com>
2026-03-30 19:42:17 +0530

    fix: update doctor validator for uploaded image save

--- backend/src/modules/doctor/validator/registerDoctor.schema.ts
+++ backend/src/modules/doctor/validator/registerDoctor.schema.ts
@@ -9,6 +9,16 @@ export const registerDoctorSchema = z.object({
     password: passwordSchema,
     medicalCertificateNumber: z.string().min(1, 'Medical certificate number is required'),
     medicalCouncilRegisterNumber: z.string().min(1, 'Medical council register number required'),
+
+    profileImage: z.string().min(1).optional(),
+    medicalCertificateImage: z.string().min(1).optional(),
+    medicalCouncilImage: z.string().min(1).optional(),
+
+    specializationDocumentKeys: z
+        .string()
+        .transform((val) => JSON.parse(val))
+        .pipe(z.array(z.string().min(1).nullable()))
+        .optional(),
     specializations: z
         .string()
         .transform((val) => JSON.parse(val))
