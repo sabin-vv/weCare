c7042d5a93b3bd3c4f8ba5d78b0e4467a60a70b0
sabin-vv <vvsabin@gmail.com>
2026-03-30 23:13:53 +0530

    feat: caregiver mapper added

--- backend/src/modules/caregiver/mapper/caregiver.mapper.ts
+++ backend/src/modules/caregiver/mapper/caregiver.mapper.ts
@@ -4,7 +4,11 @@ import { MulterFiles } from '../../auth/types/auth.types'
 import { CaregiverEntity } from '../types/caregiver.types'
 import { RegisterCaregiverDTO } from '../validator/caregiver.schema'
 
-export const toCaregiverEntity = (userId: Types.ObjectId, dto: RegisterCaregiverDTO, files: MulterFiles): CaregiverEntity => {
+export const toCaregiverEntity = (
+    userId: Types.ObjectId,
+    dto: RegisterCaregiverDTO,
+    files: MulterFiles,
+): CaregiverEntity => {
     const fromDtoOrFile = (dtoValue: string | undefined, fileField: string) => {
         return dtoValue ?? files?.[fileField]?.[0]?.originalname ?? ''
     }
