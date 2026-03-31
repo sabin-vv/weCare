335a575b1c95dcd795de796ca489e9a6049af248
sabin-vv <vvsabin@gmail.com>
2026-03-31 10:06:40 +0530

    feat: added admin routes to the router file

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,4 +1,8 @@
 import { AuthRoutes } from './modules/auth/routes/auth.routes'
+import { AdminRoutes } from './modules/admin/routes/AdminRoutes'
 import { createBrowserRouter } from 'react-router-dom'
 
-export const router = createBrowserRouter([...AuthRoutes])
+export const router = createBrowserRouter([
+    ...AuthRoutes,
+    ...AdminRoutes,
+])
