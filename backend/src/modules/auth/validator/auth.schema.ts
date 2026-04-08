13791ea8b48f5dbafada262a6c05969df3332ae2
sabin-vv <vvsabin@gmail.com>
2026-04-08 12:56:00 +0530

    feat: register schema added

--- backend/src/modules/auth/validator/auth.schema.ts
+++ backend/src/modules/auth/validator/auth.schema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 
-import { emailschema, passwordSchema } from '../../../core/validation/common.schema'
+import { emailschema, mobileSchema, nameSchema, passwordSchema } from '../../../core/validation/common.schema'
 import { UserRole } from '../types/auth.types'
 
 export const loginSchema = z.object({
@@ -13,3 +13,19 @@ export const resetPasswordSchema = z.object({
     email: emailschema,
     newPassword: passwordSchema,
 })
+
+export const registerSchema = z
+    .object({
+        name: nameSchema,
+        email: emailschema,
+        mobile: mobileSchema,
+        password: passwordSchema,
+        confirmPassword: passwordSchema,
+        role: z.enum([UserRole.DOCTOR, UserRole.CAREGIVER, UserRole.PATIENT]),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+        message: 'Passwords do not match',
+        path: ['confirmPassword'],
+    })
+
+export type registerDTO = z.infer<typeof registerSchema>
