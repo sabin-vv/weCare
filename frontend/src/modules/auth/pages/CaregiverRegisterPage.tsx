4bff524554a21a3630d6f5a56806e47f09030e2d
sabin-vv <vvsabin@gmail.com>
2026-04-08 19:02:55 +0530

    fix: update the caregiver registration page wihtout professional info

--- frontend/src/modules/auth/pages/CaregiverRegisterPage.tsx
+++ frontend/src/modules/auth/pages/CaregiverRegisterPage.tsx
@@ -1,39 +1,23 @@
 import { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 
-import { sendOtp, verifyOtp } from '../api/auth.api'
+import { register, sendOtp, verifyOtp } from '../api/auth.api'
 import OtpVerification from '../components/OtpVerification'
 import BasicInfoForm from '../form/BasicInfoForm'
-import CaregiverDetailsForm from '../form/CaregiverDetailsForm'
 import RegistrationSuccessForm from '../form/RegistrationSuccessForm'
-import { OtpPurpose } from '../types/auth.types'
-import type { CaregiverRegisterState } from '../types/caregiver.types'
+import { OtpPurpose, Role, type RegisterFormData } from '../types/auth.types'
 
 import AuthLayout from '@/layout/AuthLayout'
 import { getErrorMessage } from '@/utils/getErrorMessage'
 
 const CaregiverRegisterPage = () => {
     const [loading, setLoading] = useState(false)
-    const [registerData, setRegisterData] = useState<CaregiverRegisterState>({
-        basicInfo: {
-            name: '',
-            email: '',
-            mobile: '',
-            password: '',
-            confirmPassword: '',
-        },
-        documents: {
-            govId: null,
-            profileImage: null,
-            certificate: {
-                number: '',
-                document: null,
-            },
-            license: {
-                number: '',
-                document: null,
-            },
-        },
+    const [registerData, setRegisterData] = useState<RegisterFormData>({
+        name: '',
+        email: '',
+        mobile: '',
+        password: '',
+        confirmPassword: '',
     })
     const [step, setSteps] = useState<number>(1)
 
@@ -44,12 +28,12 @@ const CaregiverRegisterPage = () => {
         setSteps((prev) => prev - 1)
     }
 
-    const handleBasicInfoSubmit = async (data: CaregiverRegisterState['basicInfo']) => {
+    const handleBasicInfoSubmit = async (data: RegisterFormData) => {
         setLoading(true)
         try {
             setRegisterData((prev) => ({
                 ...prev,
-                basicInfo: data,
+                ...data,
             }))
             localStorage.setItem('caregiverRegister', JSON.stringify(data))
             await sendOtp(data.email, OtpPurpose.REGISTER)
@@ -64,7 +48,12 @@ const CaregiverRegisterPage = () => {
     const handleVerifyOtp = async (otp: string) => {
         setLoading(true)
         try {
-            await verifyOtp(registerData.basicInfo.email, otp)
+            await verifyOtp(registerData.email, otp)
+
+            const res = await register(registerData, Role.CAREGIVER)
+
+            toast.success(res.message)
+
             nextStep()
         } catch (error: unknown) {
             toast.error(getErrorMessage(error))
@@ -79,7 +68,7 @@ const CaregiverRegisterPage = () => {
             try {
                 setRegisterData((prev) => ({
                     ...prev,
-                    basicInfo: JSON.parse(saved),
+                    ...JSON.parse(saved),
                 }))
             } catch {
                 localStorage.removeItem('caregiverRegister')
@@ -91,7 +80,7 @@ const CaregiverRegisterPage = () => {
         <AuthLayout>
             {step === 1 && (
                 <BasicInfoForm
-                    defaultValues={registerData.basicInfo}
+                    defaultValues={registerData}
                     onSubmit={handleBasicInfoSubmit}
                     loading={loading}
                     role="caregiver"
@@ -102,26 +91,17 @@ const CaregiverRegisterPage = () => {
 
             {step === 2 && (
                 <OtpVerification
-                    email={registerData.basicInfo.email}
+                    email={registerData.email}
                     onVerify={handleVerifyOtp}
                     onResend={async () => {
-                        await sendOtp(registerData.basicInfo.email, OtpPurpose.REGISTER)
+                        await sendOtp(registerData.email, OtpPurpose.REGISTER)
                     }}
                     onBack={prevStep}
                     loading={loading}
                 />
             )}
 
-            {step === 3 && (
-                <CaregiverDetailsForm
-                    prevStep={prevStep}
-                    nextStep={nextStep}
-                    documents={registerData.documents}
-                    registerData={registerData}
-                    setRegisterData={setRegisterData}
-                />
-            )}
-            {step === 4 && <RegistrationSuccessForm />}
+            {step === 3 && <RegistrationSuccessForm />}
         </AuthLayout>
     )
 }
