cf16e416341dbebdc004f759654cb26692bf5698
sabin-vv <vvsabin@gmail.com>
2026-04-09 22:27:28 +0530

    feat: update doctor dashboard with navbar, sidebar, and form

--- frontend/src/modules/doctor/pages/DoctorDashboard.tsx
+++ frontend/src/modules/doctor/pages/DoctorDashboard.tsx
@@ -1,21 +1,16 @@
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+import DoctorDetailsForm from '../form/DoctorDetailesForm'
+
 import styles from './DoctorDashboard.module.css'
 
-import { useLogout } from '@/modules/auth/hooks/useLogout'
-import { useAuth } from '@/shared/context/AuthContext'
-
 const DoctorDashboard = () => {
-    const { user } = useAuth()
-    const handleLogout = useLogout()
-
     return (
-        <div className={styles.container}>
-            <div className={styles.card}>
-                <div className={styles.badge}>Doctor</div>
-                <h1 className={styles.heading}>Welcome, Dr. {user?.name} 👨‍⚕️</h1>
-                <p className={styles.sub}>{user?.email}</p>
-                <button className={styles.btn} onClick={handleLogout}>
-                    Logout
-                </button>
+        <div>
+            <Navbar />
+            <div className={styles.body}>
+                <Sidebar />
+                <DoctorDetailsForm />
             </div>
         </div>
     )
