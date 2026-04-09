a9f3144d02df77634d363c4a429939540366eeed
sabin-vv <vvsabin@gmail.com>
2026-04-09 17:54:31 +0530

    feat: added doctor sidebar component  with minimal data

---
+++ frontend/src/modules/doctor/components/Sidebar.tsx
@@ -0,0 +1,25 @@
+import styles from './Sidebar.module.css'
+
+import SearchField from '@/shared/components/SearchField/SearchField'
+
+const Sidebar = () => {
+    const handleSearch = () => {}
+    return (
+        <aside className={styles.sidebar}>
+            <h3 className={styles.title}>Patient Directory</h3>
+
+            <SearchField onSearch={handleSearch} />
+
+            <div className={styles.filters}>
+                <button className={styles.active}>All</button>
+                <button>Critical</button>
+                <button>High Risk</button>
+                <button>Hospitalized</button>
+            </div>
+
+            <div className={styles.list}></div>
+        </aside>
+    )
+}
+
+export default Sidebar
