2234035b0c0a06f2ab5c0e4ce1ecbbc71efe7fbf
sabin-vv <vvsabin@gmail.com>
2026-04-10 12:03:32 +0530

    fix: update the profile update router

--- backend/src/modules/doctor/routes/doctor.route.ts
+++ backend/src/modules/doctor/routes/doctor.route.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { container } from 'tsyringe'
 
 import { requireAuth } from '../../../core/middleware/requireAuth'
+import { upload } from '../../../core/middleware/upload'
 import { validate } from '../../../core/middleware/validateMiddleware'
 import { DoctorController } from '../controller/doctor.controller'
 import { DoctorSchema } from '../validator/registerDoctor.schema'
@@ -10,7 +11,7 @@ export const createDoctorRoutes = () => {
     const router = Router()
     const doctorController = container.resolve(DoctorController)
 
-    router.post('/profile', requireAuth, validate(DoctorSchema), doctorController.createProfile)
+    router.post('/profile', requireAuth, upload.none(), validate(DoctorSchema), doctorController.createProfile)
 
     return router
 }
