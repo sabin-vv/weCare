878d211a0d36f41c371ca8954049141499a897fb
sabin-vv <vvsabin@gmail.com>
2026-04-11 13:52:57 +0530

    style: update getDoctorProfile API to match updateProfile style

--- frontend/src/modules/doctor/api/doctor.api.ts
+++ frontend/src/modules/doctor/api/doctor.api.ts
@@ -1,8 +1,15 @@
+import type { DoctorProfile } from '../types/doctor.types'
+
 import type { ApiInterface } from '@/modules/auth/api/auth.api.types'
 import { api } from '@/services/api'
 
 export const updateProfile = async (data: FormData): Promise<ApiInterface> => {
     const res = await api.post('/doctors/profile', data)
+
+    return res.data
+}
+export const getDoctorProfile = async (): Promise<DoctorProfile> => {
+    const res = await api.get('/doctors/me')
 
     return res.data
 }
