01373b6652269db020b6220fc4079153925c1e3a
sabin-vv <vvsabin@gmail.com>
2026-04-11 16:03:37 +0530

    fix: updated the admin settings route

--- frontend/src/modules/doctor/routes/DoctorRoutes.tsx
+++ frontend/src/modules/doctor/routes/DoctorRoutes.tsx
@@ -1,7 +1,7 @@
 import type { RouteObject } from 'react-router-dom'
 
-import DoctorSettings from '../form/DoctorSettings'
 import DoctorDashboard from '../pages/DoctorDashboard'
+import DoctorSettings from '../pages/DoctorSettings'
 
 export const DoctorRoutes: RouteObject[] = [
     {
