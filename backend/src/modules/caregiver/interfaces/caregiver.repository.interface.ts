a98f0158084d818572147296dd7910fa8e264f81
sabin-vv <vvsabin@gmail.com>
2026-04-18 11:42:42 +0530

    add updateByUserId method to caregiver repository interface

--- backend/src/modules/caregiver/interfaces/caregiver.repository.interface.ts
+++ backend/src/modules/caregiver/interfaces/caregiver.repository.interface.ts
@@ -5,4 +5,5 @@ import { CaregiverDocument } from '../types/caregiver.types'
 export interface ICaregiverRepository {
     findByUserId(userId: Types.ObjectId): Promise<CaregiverDocument | null>
     create(data: Partial<CaregiverDocument>): Promise<CaregiverDocument>
+    updateByUserId(userId: Types.ObjectId, data: Partial<CaregiverDocument>): Promise<CaregiverDocument>
 }
