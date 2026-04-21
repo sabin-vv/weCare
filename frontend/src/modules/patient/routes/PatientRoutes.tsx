0dfd1f4966248ef3b10d148f85398736ac3a6a04
sabin-vv <vvsabin@gmail.com>
2026-04-22 00:26:13 +0530

    feat(patient): add route for /appointments/doctor/:id

--- frontend/src/modules/patient/routes/PatientRoutes.tsx
+++ frontend/src/modules/patient/routes/PatientRoutes.tsx
@@ -1,10 +1,15 @@
 import type { RouteObject } from 'react-router-dom'
 
+import DoctorAvailabilityPage from '../pages/DoctorAvailabilityPage'
 import DoctorBookingPage from '../pages/DoctorBookingPage'
 
 export const PatientRoutes: RouteObject[] = [
     {
         path: '/appointments',
         element: <DoctorBookingPage />,
+    },
+    {
+        path: '/appointments/doctor/:doctorId',
+        element: <DoctorAvailabilityPage />,
     },
 ]
