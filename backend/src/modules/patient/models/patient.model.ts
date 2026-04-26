4bf5fc5a41d24a4c9c325005408042fe2aadd621
sabin-vv <vvsabin@gmail.com>
2026-04-26 18:07:49 +0530

    removed the mobile fiels in patient schema, it already exist in user schema

--- backend/src/modules/patient/models/patient.model.ts
+++ backend/src/modules/patient/models/patient.model.ts
@@ -22,10 +22,6 @@ const patientSchema = new Schema<PatientDocument>(
             type: String,
             required: true,
         },
-        mobile: {
-            type: String,
-            required: true,
-        },
         isActive: {
             type: Boolean,
             required: true,
