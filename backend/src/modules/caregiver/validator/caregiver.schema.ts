e4a1d01f77fa074b66cc76148b1f23120c361451
sabin-vv <vvsabin@gmail.com>
2026-04-29 20:08:09 +0530

    fix: removed register schema , that not needed

--- backend/src/modules/caregiver/validator/caregiver.schema.ts
+++ backend/src/modules/caregiver/validator/caregiver.schema.ts
@@ -1,23 +1,6 @@
 import z from 'zod'
 
-import { emailschema, mobileSchema, nameSchema, passwordSchema } from '../../../core/validation/common.schema'
-
-export const registerCaregiverSchema = z.object({
-    name: nameSchema,
-    email: emailschema,
-    mobile: mobileSchema,
-    password: passwordSchema,
-    certificateNumber: z.string().min(1, 'certificate number is required'),
-    licenseNumber: z.string().min(1, 'License number is required'),
-
-    govIdImage: z.string().min(1).optional(),
-    profileImage: z.string().min(1).optional(),
-    certificateImage: z.string().min(1).optional(),
-    licenseImage: z.string().min(1).optional(),
-})
-
 export const createCaregiverProfileSchema = z.object({
-    email: emailschema.optional(),
     certificateNumber: z.string().min(1, 'Certificate number is required'),
     licenseNumber: z.string().min(1, 'License number is required'),
     govIdImage: z.string().min(1, 'Government ID is required'),
@@ -26,5 +9,4 @@ export const createCaregiverProfileSchema = z.object({
     licenseImage: z.string().min(1, 'License document is required'),
 })
 
-export type RegisterCaregiverDTO = z.infer<typeof registerCaregiverSchema>
 export type CreateCaregiverProfileDTO = z.infer<typeof createCaregiverProfileSchema>
