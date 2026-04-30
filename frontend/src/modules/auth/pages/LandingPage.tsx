6c07eca8ae901e759b1914aaec9245d8f4a72abd
sabin-vv <vvsabin@gmail.com>
2026-04-30 19:02:48 +0530

    fix: update landing page with updated header

--- frontend/src/modules/auth/pages/LandingPage.tsx
+++ frontend/src/modules/auth/pages/LandingPage.tsx
@@ -1,11 +1,10 @@
 import { useNavigate } from 'react-router-dom'
 
-import Header from '../components/Header'
-
 import styles from './LandingPage.module.css'
 
 import Button from '@/shared/components/Button/Button'
 import Footer from '@/shared/components/Footer/Footer'
+import Header from '@/shared/components/Header/Header'
 
 const LandingPage = () => {
     const navigate = useNavigate()
