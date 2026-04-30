e90c175bfae1dd902c68475c0fc28cc17ed2a4b2
sabin-vv <vvsabin@gmail.com>
2026-04-30 19:02:16 +0530

    fix: update the style ad

---
+++ frontend/src/shared/components/Header/Header.types.ts
@@ -0,0 +1,25 @@
+import type { ReactNode } from 'react'
+
+export interface NavLink {
+    label: string
+    path: string
+}
+
+export interface HeaderProps {
+    titlePrefix?: string
+    subtitle?: string
+    navLinks?: NavLink[]
+    children?: ReactNode
+}
+
+export interface navLinkRight {
+    notification: string
+    settings: string
+}
+
+export interface RoleRoute {
+    doctor: navLinkRight
+    caregiver: navLinkRight
+    patient: navLinkRight
+    admin: navLinkRight
+}
