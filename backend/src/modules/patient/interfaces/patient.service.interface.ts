77deaafc84307088bafe209bdf51e7edbedfe9ed
sabin-vv <vvsabin@gmail.com>
2026-05-05 09:17:06 +0530

    feat: update patient.service.interface.ts

--- backend/src/modules/patient/interfaces/patient.service.interface.ts
+++ backend/src/modules/patient/interfaces/patient.service.interface.ts
@@ -1,4 +1,5 @@
-import { PatientProfileResponseDTO, PatientResponseDTO } from '../mapper/patient.mapper'
+import { PatientResponseDTO } from '../mapper/patient.mapper'
+import { ListPatientsResponse, PatientProfileResponseDTO } from '../types/patient.types'
 import { RegisterPatientDTO } from '../validator/patient.schema'
 import { UpdatePatientSettingsDTO } from '../validator/updatePatientSettings.schema'
 
@@ -6,4 +7,14 @@ export interface IPatientService {
     registerPatient(dto: RegisterPatientDTO): Promise<PatientResponseDTO>
     getProfile(userId: string): Promise<PatientProfileResponseDTO>
     updateProfile(userId: string, dto: UpdatePatientSettingsDTO): Promise<PatientProfileResponseDTO>
+
+    listPatients(
+        doctorId: string,
+        params: {
+            search: string
+            filter: string
+            page: number
+            limit: number
+        },
+    ): Promise<ListPatientsResponse>
 }
