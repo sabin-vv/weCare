944434b826c43b686510a709e19ce9e6ffedc972
sabin-vv <vvsabin@gmail.com>
2026-05-08 19:58:15 +0530

    refactor: remove caregiver sidebar component and update layout

--- frontend/src/layout/CaregiverLayout.tsx
+++ frontend/src/layout/CaregiverLayout.tsx
@@ -3,14 +3,12 @@ import { Outlet } from 'react-router-dom'
 import styles from './CaregiverLayout.module.css'
 
 import CaregiverHeader from '@/modules/caregiver/components/CaregiverHeader'
-import CaregiverSidebar from '@/modules/caregiver/components/CaregiverSidebar'
 
 const CaregiverLayout = () => {
     return (
         <div className={styles.shell}>
             <CaregiverHeader />
             <div className={styles.body}>
-                <CaregiverSidebar />
                 <main className={styles.main}>
                     <div className={styles.content}>
                         <Outlet />
