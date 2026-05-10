57c720ad17ad4003679cac66722c3dce5e790c14
sabin-vv <vvsabin@gmail.com>
2026-05-10 17:55:23 +0530

    feat: findById repository interface added

--- backend/src/modules/patient/interfaces/patient.repository.interface.ts
+++ backend/src/modules/patient/interfaces/patient.repository.interface.ts
@@ -11,6 +11,7 @@ export interface ListPatientParams {
 }
 
 export interface IPatientRepository {
+    findById(id: string): Promise<PatientDocument | null>
     findByUserId(userId: Types.ObjectId): Promise<PatientDocument | null>
     updateByUserId(userId: Types.ObjectId, data: Partial<PatientDocument>): Promise<PatientDocument | null>
     create(data: Partial<PatientDocument>): Promise<PatientDocument>
