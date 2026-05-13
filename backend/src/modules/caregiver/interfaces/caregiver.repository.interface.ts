3de5f024185641a60f43ea919d8f91c76d98de40
sabin-vv <vvsabin@gmail.com>
2026-05-14 01:54:49 +0530

    Add findById and findAllActive to caregiver repository interface

--- backend/src/modules/caregiver/interfaces/caregiver.repository.interface.ts
+++ backend/src/modules/caregiver/interfaces/caregiver.repository.interface.ts
@@ -4,6 +4,8 @@ import { CaregiverDocument } from '../types/caregiver.types'
 
 export interface ICaregiverRepository {
     findByUserId(userId: Types.ObjectId): Promise<CaregiverDocument | null>
+    findById(id: string): Promise<CaregiverDocument | null>
+    findAllActive(search?: string): Promise<CaregiverDocument[]>
     create(data: Partial<CaregiverDocument>): Promise<CaregiverDocument>
     updateByUserId(userId: Types.ObjectId, data: Partial<CaregiverDocument>): Promise<CaregiverDocument | null>
 }
