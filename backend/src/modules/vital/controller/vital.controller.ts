c19901cae677cb60e315ca5c6b7c76a23838e173
sabin-vv <vvsabin@gmail.com>
2026-06-03 19:04:13 +0530

    refactor(vital): remove createVital and getPatientVitals handlers

--- backend/src/modules/vital/controller/vital.controller.ts
+++ backend/src/modules/vital/controller/vital.controller.ts
@@ -5,38 +5,11 @@ import { TOKENS } from '../../../container/tokens'
 import { HTTP_STATUS } from '../../../core/constants/httpStatus'
 import { AppError } from '../../../core/errors/AppError'
 import { IVitalService } from '../interfaces/vital.service.interface'
-import { CreateVitalDTO, CreateVitalPlanDTO } from '../validator/vital.schema'
+import { CreateVitalPlanDTO } from '../validator/vital.schema'
 
 @injectable()
 export class VitalController {
     constructor(@inject(TOKENS.IVitalService) private _vitalService: IVitalService) {}
-
-    createVital = async (req: Request, res: Response) => {
-        const userId = req.user?.userId
-        if (!userId) {
-            throw new AppError(HTTP_STATUS.UNAUTHORIZED, 'User not authenticated')
-        }
-
-        const dto: CreateVitalDTO = req.body
-        const vital = await this._vitalService.createVital(userId, dto)
-
-        res.status(HTTP_STATUS.CREATED).json({
-            success: true,
-            data: vital,
-        })
-    }
-
-    getPatientVitals = async (req: Request, res: Response) => {
-        const { patientId } = req.params as { patientId: string }
-        const { type } = req.query
-
-        const vitals = await this._vitalService.getPatientVitals(patientId, type as string | undefined)
-
-        res.status(HTTP_STATUS.OK).json({
-            success: true,
-            data: vitals,
-        })
-    }
 
     createVitalPlan = async (req: Request, res: Response) => {
         const userId = req.user?.userId
