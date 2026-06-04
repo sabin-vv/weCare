35dee6cdf35203d380ac5cba4367fd6bbc07e0f1
sabin-vv <vvsabin@gmail.com>
2026-06-04 20:21:48 +0530

    feat(alert): implement AlertRepository

---
+++ backend/src/modules/alert/repository/alert.repository.ts
@@ -0,0 +1,23 @@
+import { Types } from 'mongoose'
+import { injectable } from 'tsyringe'
+
+import { BaseRepository } from '../../../core/base/base.repository'
+import { IAlertRepository } from '../interfaces/alert.repository.interface'
+import { alertModel } from '../models/alert.model'
+import { AlertDocument } from '../types/alert.types'
+
+@injectable()
+export class AlertRepository extends BaseRepository<AlertDocument> implements IAlertRepository {
+    constructor() {
+        super(alertModel)
+    }
+
+    async findByPatientIds(patientIds: string[], filter: Record<string, unknown> = {}): Promise<AlertDocument[]> {
+        return this.model
+            .find({
+                patientId: { $in: patientIds.map((id) => new Types.ObjectId(id)) },
+                ...filter,
+            })
+            .sort({ severity: -1, triggeredAt: -1 })
+    }
+}
