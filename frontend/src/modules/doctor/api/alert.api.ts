c03c408e7650f87d22aff5fc1a1f2028e74664bc
sabin-vv <vvsabin@gmail.com>
2026-06-05 18:46:48 +0530

    feat: add alert API service functions (getAlerts, acknowledgeAlert)

---
+++ frontend/src/modules/doctor/api/alert.api.ts
@@ -0,0 +1,16 @@
+import { api } from '@/services/api'
+import { ALERTS_API } from '@/shared/constants/api.constants'
+
+import type { AlertData, AlertsResponse, AcknowledgeResponse } from '../types/doctor.types'
+
+export const getAlerts = async (
+    filters?: { type?: string; severity?: string; status?: string },
+): Promise<AlertData[]> => {
+    const res = await api.get<AlertsResponse>(ALERTS_API, { params: filters })
+    return res.data.data
+}
+
+export const acknowledgeAlert = async (alertId: string, note?: string): Promise<AlertData> => {
+    const res = await api.patch<AcknowledgeResponse>(`${ALERTS_API}/${alertId}/acknowledge`, { note })
+    return res.data.data
+}
