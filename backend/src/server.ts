4bc680006dd7c12ee62c010af51f1abbac3b64be
sabin-vv <vvsabin@gmail.com>
2026-06-11 14:10:06 +0530

    feat: initialize socket server with HTTP server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,14 @@
 import 'reflect-metadata'
 import './container'
 
+import http from 'http'
+
 import app from './app'
 import { connectDB } from './core/config/db'
 import { env } from './core/config/env'
 import { initializeCrons } from './core/cron'
 import { logger } from './core/logger/logger'
+import { initializeSocketServer } from './core/socket'
 
 const PORT = env.PORT
 
@@ -13,7 +16,10 @@ const startServer = async () => {
     await connectDB()
     initializeCrons()
 
-    app.listen(PORT, () => {
+    const server = http.createServer(app)
+    initializeSocketServer(server)
+
+    server.listen(PORT, () => {
         logger.info(`Server running at port ${PORT}`)
     })
 }
