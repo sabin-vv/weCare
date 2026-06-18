59cd09abbaaf8dcb4f4fa80212fd1b7bcd6ee04d
sabin-vv <vvsabin@gmail.com>
2026-06-18 12:15:09 +0530

    feat: register /api/medical-records route in app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,6 +7,7 @@ import { createAdminRoutes } from './modules/admin/routes/admin.route'
 import { createAlertRoutes } from './modules/alert/routes/alert.route'
 import { createAppointmentRoutes } from './modules/appointment/routes/appointment.route'
 import { createActivityLogRoutes } from './modules/activityLog/routes/activityLog.routes'
+import { createMedicalRecordRoutes } from './modules/medicalRecord/routes/medicalRecord.route'
 import { createAuthRoutes } from './modules/auth/routes/auth.route'
 import { createCaregiverRoutes } from './modules/caregiver/routes/caregiver.route'
 import { createCaregiverActivityRoutes } from './modules/caregiverActivity/routes/caregiverActivity.route'
@@ -74,6 +75,8 @@ app.use('/api/feedback', createFeedbackRoutes())
 app.use('/api/uploads', createUploadsRoutes())
 
 app.use('/api/notifications', createNotificationRoutes())
+
+app.use('/api/medical-records', createMedicalRecordRoutes())
 
 app.use('/api/activity-logs', createActivityLogRoutes())
 
