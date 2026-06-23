52620686603098def97a5d719dcf1d244b596a79
sabin-vv <vvsabin@gmail.com>
2026-06-23 19:15:11 +0530

    feat(caregiver): add assignedDoctorName to PatientSummary interface

--- backend/src/modules/caregiver/interfaces/caregiver.repository.interface.ts
+++ backend/src/modules/caregiver/interfaces/caregiver.repository.interface.ts
@@ -12,6 +12,8 @@ export interface PatientSummary {
     riskLevel: string
     clinicalStatus: string
     profileImage?: string
+    primaryDoctorId?: Types.ObjectId
+    assignedDoctorName?: string
 }
 
 export interface ICaregiverRepository {
