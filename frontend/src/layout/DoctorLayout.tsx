3074302f595413d44da0d5c9f4b8bf50d63e902f
sabin-vv <vvsabin@gmail.com>
2026-06-26 00:02:21 +0530

    refactor(layout): change DoctorLayout to use Outlet instead of children

--- frontend/src/layout/DoctorLayout.tsx
+++ frontend/src/layout/DoctorLayout.tsx
@@ -1,19 +1,22 @@
-import type { ReactNode } from 'react'
+import { Outlet } from 'react-router-dom'
 
 import styles from './DoctorLayout.module.css'
 
 import DoctorHeader from '@/modules/doctor/components/DoctorHeader'
 import Footer from '@/shared/components/Footer/Footer'
 
-const DoctorLayout = ({ children }: { children: ReactNode }) => {
+const DoctorLayout = () => {
     return (
         <div className={styles.Wrapper}>
             <DoctorHeader />
             <div className={styles.body}>
-                <main className={styles.main}>{children}</main>
+                <main className={styles.main}>
+                    <Outlet />
+                </main>
             </div>
             <Footer />
         </div>
     )
 }
+
 export default DoctorLayout
