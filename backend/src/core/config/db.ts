115542d9864452f92923a775c873820a0db47d29
sabin-vv <vvsabin@gmail.com>
2026-06-26 17:50:24 +0530

    fix(db): add serverSelectionTimeoutMS and remove hardcoded env defaults

--- backend/src/core/config/db.ts
+++ backend/src/core/config/db.ts
@@ -4,7 +4,9 @@ import { env } from './env'
 
 export const connectDB = async (): Promise<void> => {
     try {
-        await mongoose.connect(env.MONGO_URI)
+        await mongoose.connect(env.MONGO_URI!, {
+            serverSelectionTimeoutMS: 1000,
+        })
     } catch (error: unknown) {
         console.error(`database connection failed : ${error}`)
         process.exit(1)
--- backend/src/core/config/env.ts
+++ backend/src/core/config/env.ts
@@ -4,8 +4,7 @@ dotenv.config({ quiet: true })
 
 export const env = {
     PORT: process.env.PORT || '5000',
-    MONGO_URI: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/weCare',
-    JWT_SECRET: process.env.JWT_SECRET || '',
+    MONGO_URI: process.env.MONGODB_URI,
     EMAIL_USER: process.env.EMAIL_USER || 'vvsabin@gmail.com',
     EMAIL_PASSWORD: process.env.EMAIL_PASSWORD || 'uinkkccprbykwpdf',
     JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET || '',
